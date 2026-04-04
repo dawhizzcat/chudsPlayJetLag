7eab506015a7cf37fb487f7d507748986617fcee
dawhizzcat <105724019+dawhizzcat@users.noreply.github.com>
2026-04-04 00:25:19 -0400

    Update api.js

--- api.js
+++ api.js
@@ -5,17 +5,30 @@ async function apiPost(path, data) {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "ngrok-skip-browser-warning": "true"
+      "ngrok-skip-browser-warning": "123",
+      "User-Agent": "Mozilla/5.0"
     },
     body: JSON.stringify(data)
   });
   return res.json();
 }
+
+
 async function apiGet(path) {
   const res = await fetch(baseUrl + path, {
     headers: {
-      "ngrok-skip-browser-warning": "true"
+        "ngrok-skip-browser-warning": "123",
+        "User-Agent": "Mozilla/5.0"
     }
   });
-  return res.json();
+
+  const text = await res.text();
+  console.log("RAW RESPONSE:", text);
+
+  try {
+    return JSON.parse(text);
+  } catch (e) {
+    console.error("NOT JSON:", text);
+    throw e;
+  }
 }
