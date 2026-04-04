c03ef098cf25169ae36e5ec20a9ae759806c0d1f
dawhizzcat <105724019+dawhizzcat@users.noreply.github.com>
2026-04-04 00:15:49 -0400

    Update api.js

--- api.js
+++ api.js
@@ -3,13 +3,19 @@ const baseUrl = "https://wearifully-undeniable-emmett.ngrok-free.dev";
 async function apiPost(path, data) {
   const res = await fetch(baseUrl + path, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+      "ngrok-skip-browser-warning": "true"
+    },
     body: JSON.stringify(data)
   });
   return res.json();
 }
-
 async function apiGet(path) {
-  const res = await fetch(baseUrl + path);
+  const res = await fetch(baseUrl + path, {
+    headers: {
+      "ngrok-skip-browser-warning": "true"
+    }
+  });
   return res.json();
 }
